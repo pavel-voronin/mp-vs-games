6c57f253f999bdce3a30ee0ce8006717e504156e
Pavel Voronin <pavel.a.voronin@gmail.com>
2023-10-24 18:17:47 +0700

    Updates

---
+++ _nuxt/index.79ef1a49.js
@@ -0,0 +1,1 @@
+import{f as e,n}from"./entry.c71ff45e.js";const o=e({__name:"index",setup(t){return n("/roulette/win-rate"),()=>{}}});export{o as default};
---
+++ _nuxt/index.e9c477d9.js
@@ -0,0 +1,1 @@
+import{f as e,n}from"./entry.c71ff45e.js";const a=e({__name:"index",setup(t){return n("/roulette"),()=>{}}});export{a as default};
